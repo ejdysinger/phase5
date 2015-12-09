2e88bde99f97be75682db5aac8e9cff1a9a6a029
ejdysinger@gmail.com <ejdysinger@gmail.com>
2015-12-09 16:11:45 -0700

    pulling to debug

--- phase5.c
+++ phase5.c
@@ -17,7 +17,6 @@ extern void mbox_condsend(systemArgs *args_ptr);
 extern void mbox_condreceive(systemArgs *args_ptr);
 void * vmInitReal(int mappings, int pages, int frames, int pagers);
 static int Pager(char *buf);
-void vmDestroyReal(void);
 
 Process processes[MAXPROC];
 int vmInitialized = 0;
@@ -180,7 +179,6 @@ static void
 vmDestroy(systemArgs *sysargsPtr)
 {
    CheckMode();
-    vmDestroyReal();
 } /* vmDestroy */
 
 
@@ -232,28 +230,18 @@ vmInitReal(int mappings, int pages, int frames, int pagers)
    }
 
    // malloc the frame table, initialize each frame within it and store its dimension
+   frameTable = malloc(frames * sizeof(FTE));
    frameTableSize = frames;
-    
-    frameTable = malloc(sizeof(FTE));
-    frameTable->frame = 0;
-    frameTable->next = NULL;
-    frameTable->page = -1;
-    frameTable->procNum = -1;
-    frameTable->state = FR_UNUSED;
-
-    FTE *current = frameTable;
-    FTE *temp;
-   for(i = 1; i < frames; i++, current = current->next){
-        temp = malloc(sizeof(FTE));
-       
-	   // each frame gets a number
-       temp->frame = i;
-       temp->next = NULL;
-       temp->page = -1;
-       temp->state = FR_UNUSED;
-       temp->procNum = -1;
+   for(i = 0; i < frames; i++){
+	   // each frame gets an address in memory, starting at the top of the VM region
+	   frameTable[i]->frame = i*USLOSS_MmuPageSize() + USLOSS_MmuRegion(NULL);
 	   // assign the next pointer
-       current->next = temp;
+	   if(i == frames - 1)
+		   frameTable[i]->next = NULL;
+	   else
+		   frameTable[i]->next = frameTable[i+1];
+	   frameTable[i]->page = -1;
+	   frameTable[i]->state = FR_UNUSED;
    }
 
    //. create disk occupancy table and calculate global disk params based on size of pages from MMU
@@ -287,9 +275,9 @@ vmInitReal(int mappings, int pages, int frames, int pagers)
    memset((char *) &vmStats, 0, sizeof(VmStats));
    vmStats.pages = pages;
    vmStats.frames = frames;
-    vmStats.diskBlocks = numBlocks;
+   if ((i = DiskSize(1, &sec, &track, &disk)) != -1)
+	   vmStats.diskBlocks = disk / USLOSS_MmuPageSize();
    vmStats.freeDiskBlocks = vmStats.diskBlocks;
-    vmStats.freeFrames = frames;
 
    return USLOSS_MmuRegion(&dummy);
 } /* vmInitReal */
@@ -504,7 +492,7 @@ Pager(char *buf)
 						// copy the page's contents into the pager daemon's buffer
 						memcpy(pageBuff,frameTable[clockHand]->page + USLOSS_MmuRegion(), USLOSS_MmuPageSize());
 						// write the page contents to disk
-						pgPtr->diskBlock = pageDiskWrite(pageBuff, pageNum);
+						pgPtr->diskBlock = pageDiskWrite(pageBuff);
 						pgPtr->state = INDISK;
 					}
 					// if the page has information stored on disk, write it to memory
